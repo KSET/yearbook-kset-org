2807cc3628fa75254ca318715c8774f09600b0bf
Stanko Krtalić <stanko.krtalic@gmail.com>
2016-04-09 14:17:18 +0200

    New version

--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,7 @@
 
 var yearbookControllers = angular.module('yearbookControllers', []);
 
-yearbookControllers.controller('SectionsListCtrl', ['$scope', function ($scope) {
+yearbookControllers.controller('SectionsListCtrl', ['$scope','$window', function ($scope, $window) {
 
     $scope.sections = [
         { "name": "BIKE", "anchor": "bike-sekcija" },
@@ -16,18 +16,31 @@ yearbookControllers.controller('SectionsListCtrl', ['$scope', function ($scope) 
         {"name": "VIDEO", "anchor": "video-sekcija"}
     ];
 
+    $scope.goToAnchor = function (anchor) {
+        console.log($window.location.hash);
+        var newHash = "#sections/" + anchor;
+        console.log(newHash);
+        $window.location.hash = newHash;
+    }
    
 }]);
 
 
-yearbookControllers.controller('MembersCtrl', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
+yearbookControllers.controller('MembersCtrl', ['$scope', '$http', '$timeout','$window', function($scope, $http, $timeout, $window) {
     
     $http.get('/ksetovci/ksetovci.json').success(function(data) {
         $scope.members = data;
         $timeout(function () {
             $('#fullpage').fullpage({
-            anchors:['sections']});
+                anchors: ['sections'],
+                scrollingSpeed: 1000
+            });
         }, 0);
     });
+
+    $scope.goToTOC = function () {
+        var newHash = "#sections/table-of-contents";
+        $window.location.hash = newHash;
+    }
 
 }]);
